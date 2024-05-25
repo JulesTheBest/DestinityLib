0a825d9b8a393bb1f4b0c95e7c01f193561fd36a
Jules <167021550+JulesTheBest@users.noreply.github.com>
2024-05-25 19:48:55 +0200

    Version 0.1
    Add :
    
    - log()
    - delay()
    - sameTime()

---
+++ DestinityLib.js
@@ -0,0 +1,29 @@
+// Version 0.1
+// MIT License - Copyright (c) 2024 Jules
+
+// Check new releases / documentation on https://github.com/JulesTheBest/DestinityLib
+
+function delay(time) {
+
+    const t = Date.now()
+    while(Date.now() - t < time) {
+
+
+    }
+
+
+}
+
+function sameTime(func) {
+
+    setTimeout(() => {func}, 0)
+
+
+}
+
+function log(message) {
+
+    console.log(message)
+
+
+}
