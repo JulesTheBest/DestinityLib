f9eb0f6a1c975f573cbd3e0696244038347961ad
Jules <167021550+JulesTheBest@users.noreply.github.com>
2024-06-02 09:56:18 +0200

    Add files via upload

--- DestinityLib.js
+++ DestinityLib.js
@@ -3,6 +3,36 @@
 
 // Check new releases / documentation on https://github.com/JulesTheBest/DestinityLib
 
+
+// Var / Const
+
+
+backLoopError = false
+
+
+// Class
+
+
+class BackLoop {
+    func = function a(){}
+}
+
+class Log {
+
+    error(message){
+        throw new Error(message)
+    }
+
+    warn(message){
+        console.warn(message)
+    }
+
+}
+
+
+// Functions 
+
+
 function delay(time) {
 
     const t = Date.now()
@@ -27,3 +57,40 @@ function log(message) {
 
 
 }
+
+function average(array) {
+
+    const a =array.reduce((sum, current) => {
+        return sum += current
+    })
+
+    const b = array.length
+
+    return c = a/b
+
+}
+
+function median(array) {
+
+    const s = [...array].sort((a, b) => a - b)
+    const m = Math.floor(s.length / 2)
+    return s.length % 2 === 0
+    ? (s[m] + s[m - 1]) / 2
+    : s[m]
+
+
+}
+
+function backLoopRun() {
+    if (typeof(BackLoop.func) !== 'function'){
+        if(backLoopError === false){
+            backLoopError = true
+            throw new Error("501 : Badly defined BackLoop")
+        }
+    } else{BackLoop.func()}
+}
+
+
+// Other
+
+setInterval(() => {backLoopRun()}, 5)
